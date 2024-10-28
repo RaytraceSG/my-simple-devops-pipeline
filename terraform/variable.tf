575197f84b6f6c5c6ccfcf167eaabaa679b9a286
RaytraceSG <azmi.maniku@gmail.com>
2024-10-28 22:09:41 +0900

    update variable.tf to trigger workflow

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,5 +1,5 @@
 variable "bucket_name" {
-  description = "Region for S3 Bucket"
+  description = "Region for S3 Bucket1"
   type        = string
   default     = "azmi1-s3-devops-bucket-20241028"
 }
